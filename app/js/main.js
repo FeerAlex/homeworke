1a82c1c697e302b4f74f5959e18f5a7185d88d02
Aleksandr Feer <pertik@list.ru>
2015-10-05 12:15:31 +0300

    12-ый коммит - поправки в css

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,54 @@
-$(document).ready(function() {
-	console.log('я на главной странице');
-});
+// Поле с файлом через javascript + обрезание лишнего
+window.onload = function () {
+	document.getElementById("project_picture").onchange = function (e) {
+		var str = e.target.value;
+		if (str.lastIndexOf('\\')){
+			var i = str.lastIndexOf('\\')+1;
+		}
+		else{
+		    var i = str.lastIndexOf('/')+1;
+		}	
+		document.getElementById("fileformlabel").innerHTML = str.slice(i);
+	}
 
-function getName (str){
-    if (str.lastIndexOf('\\')){
-        var i = str.lastIndexOf('\\')+1;
-    }
-    else{
-        var i = str.lastIndexOf('/')+1;
-    }						
-    var filename = str.slice(i);			
-    var uploaded = document.getElementById("fileformlabel");
-    uploaded.innerHTML = filename;
+    // document.getElementById('button_submit_succesful').onclick = function(e) {
+    //     if ($('#project_name').val() == "") {
+    //         e.preventDefault();
+    //         alert("Вы не указали имя проекта!");
+    //     };
+
+    //     if ($('#project_picture').val() == "") {
+    //         e.preventDefault();
+    //         alert("Вы не выбрали изображение");
+    //     };
+
+    //     if ($('#project_url').val() == "") {
+    //         e.preventDefault();
+    //         alert("Вы не указали url проекта");
+    //     };
+
+    //     if ($('#project_info').val() == "") {
+    //         e.preventDefault();
+    //         alert("Вы не указали информацию о проекте");
+    //     };
+    // };
+
+    // Фокус и потеря фокуса 
+    // $('#project_name').focus(function(){$(this).addClass('onfocus');}).blur(function(){$(this).removeClass('onfocus')});
+    // $('#project_picture').focus(function(){$(this).addClass('onfocus');}).blur(function(){$(this).removeClass('onfocus')});
+    // $('#project_url').focus(function(){$(this).addClass('onfocus');}).blur(function(){$(this).removeClass('onfocus')});
+    // $('#project_info').focus(function(){$(this).addClass('onfocus');}).blur(function(){$(this).removeClass('onfocus')});
+
 }
+
+// Поле с файлом через jQuery
+// $(document).ready(function() {
+// 	$( "#project_picture" ).change(function() {
+// 		var $this = $(this),
+// 			val = $this.val();
+// 		$('#fileformlabel').text(val);
+// 	});
+// });
 
 // Всплывающий попап
 ;(function($) {
